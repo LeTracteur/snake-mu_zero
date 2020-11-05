66bc31ac0bf8d3631fdc5c4a41aeed86c865fc99
AntoineRichard <antoine.richard@gatech.edu>
2020-11-05 18:44:24 +0100

    changing mcts to an implementation closer from the pseudo code

--- muzero_mcts.py
+++ muzero_mcts.py
@@ -4,105 +4,91 @@ import tensorflow as tf
 import math
 
 
-class MCTS:
-    def __init__(self, settings):
-        self.sts = settings
+#class MCTS:
+#    def __init__(self, settings):
+#        self.sts = settings
 
-    def run(self, model, observation, legal_actions, to_play, add_exploration_noise):
-        """
-        At the root of the search tree we use the representation function to obtain a
-        hidden state given the current observation.
-        We then run a Monte Carlo Tree Search using only action sequences and the model
-        learned by the network.
-        """
-        root = Node(0)
-        #observation = (torch.tensor(observation).float().unsqueeze(0).to(next(model.parameters()).device))
-        root_pred_value, reward, policy_logits, hidden_state = model.initial_inference(np.expand_dims(observation, 0))
-        root_pred_value = support_to_scalar(root_pred_value, self.sts.support_size)
-        #reward = support_to_scalar(reward, self.sts.support_size)
+def run_mcts(settings, root, action, model, to_play, training):
+    """
+    At the root of the search tree we use the representation function to obtain a
+    hidden state given the current observation.
+    We then run a Monte Carlo Tree Search using only action sequences and the model
+    learned by the network.
+    """
+    #root = Node(0)
+    #observation = (torch.tensor(observation).float().unsqueeze(0).to(next(model.parameters()).device))
+    #root_pred_value, reward, policy_logits, hidden_state = model.initial_inference(np.expand_dims(observation, 0))
+    #root_pred_value = support_to_scalar(root_pred_value, self.sts.support_size)
+    #reward = support_to_scalar(reward, self.sts.support_size)
+    #reward = reward.numpy().item()
+    #root.expand(legal_actions, to_play, reward, policy_logits, hidden_state)
+    #if add_exploration_noise:
+    #    root.add_exploration_noise(
+    #        dirichlet_alpha=self.sts.root_dirichlet_alpha,
+    #        exploration_fraction=self.sts.root_exploration_fraction,
+    #    )
+
+    min_max_stats = MinMaxStats()
+    for _ in range(settings.num_simulations):
+        node = root
+        search_path = [node]
+
+        while node.expanded():
+            action, node = select_child(settings, node, min_max_stats)
+            search_path.append(node)
+
+        # Inside the search tree we use the dynamics function to obtain the next hidden
+        # state given an action and the previous hidden state
+        parent = search_path[-2]
+        value, reward, policy_logits, hidden_state = model.recurrent_inference(parent.hidden_state, action, training=training)
+        # Cast
+        value = support_to_scalar(value, settings.support_size)
         reward = reward.numpy().item()
-        root.expand(legal_actions, to_play, reward, policy_logits, hidden_state)
-        if add_exploration_noise:
-            root.add_exploration_noise(
-                dirichlet_alpha=self.sts.root_dirichlet_alpha,
-                exploration_fraction=self.sts.root_exploration_fraction,
-            )
+        # expand
+        expand_node(node, settings.action_pos, True, reward, policy_logits, hidden_state)
+        backpropagate(search_path, value, True, settings.discount, min_max_stats)
 
-        min_max_stats = MinMaxStats()
+    return root
 
-        max_tree_depth = 0
-        for _ in range(self.sts.num_simulations):
-            virtual_to_play = to_play
-            node = root
-            search_path = [node]
-            current_tree_depth = 0
+def backpropagate(search_path, value, to_play, discount, min_max_stats):
+    """
+    At the end of a simulation, we propagate the evaluation all the way up the tree
+    to the root.
+    """
+    for node in reversed(search_path):
+        node.value_sum += value if node.to_play == to_play else -value
+        node.visit_count += 1
+        min_max_stats.update(node.value())
+        value = node.reward + discount * value
 
-            while node.expanded():
-                current_tree_depth += 1
-                action, node = self.select_child(node, min_max_stats)
-                search_path.append(node)
-
-                # Players play turn by turn
-                if virtual_to_play + 1 < len(self.sts.players):
-                    virtual_to_play = self.sts.players[virtual_to_play + 1]
-                else:
-                    virtual_to_play = self.sts.players[0]
-
-            # Inside the search tree we use the dynamics function to obtain the next hidden
-            # state given an action and the previous hidden state
-            parent = search_path[-2]
-            value, reward, policy_logits, hidden_state = model.recurrent_inference(parent.hidden_state, action)
-            value = support_to_scalar(value, self.sts.support_size)
-            #reward = support_to_scalar(reward, self.sts.support_size)
-            reward = reward.numpy().item()
-            node.expand(self.sts.action_pos, virtual_to_play, reward, policy_logits, hidden_state)
-
-            self.backpropagate(search_path, value, virtual_to_play, min_max_stats)
-
-            max_tree_depth = max(max_tree_depth, current_tree_depth)
-
-        return root, max_tree_depth
-
-    def select_child(self, node, min_max_stats):
-        """
-        Select the child with the highest UCB score.
-        """
-        #scores = [self.ucb_score(node, child, min_max_stats) for action, child in node.children.items()]
-        #exp = np.exp(scores - np.max(scores))
-        #prob = exp / exp.sum()
-        #action = np.random.choice([action for action, child in node.children.items()], p = prob)
-        max_ucb = max([self.ucb_score(node, child, min_max_stats) for action, child in node.children.items()])
-        action = np.random.choice([action for action, child in node.children.items() if self.ucb_score(node, child, min_max_stats) == max_ucb])
-        return action, node.children[action]
-
-    def ucb_score(self, parent, child, min_max_stats):
-        """
-        The score for a node is based on its value, plus an exploration bonus based on the prior.
-        """
-        pb_c = (math.log((parent.visit_count + self.sts.pb_c_base + 1) / self.sts.pb_c_base) + self.sts.pb_c_init)
-        pb_c *= math.sqrt(parent.visit_count) / (child.visit_count + 1)
-
-        prior_score = pb_c * child.prior
-
-        if child.visit_count > 0:
-            # Mean value Q
-            value_score = min_max_stats.normalize(child.reward + self.sts.discount * child.value())
-        else:
-            value_score = 0
-
-        return prior_score + value_score
-
-    def backpropagate(self, search_path, value, to_play, min_max_stats):
-        """
-        At the end of a simulation, we propagate the evaluation all the way up the tree
-        to the root.
-        """
-        for node in reversed(search_path):
-            node.value_sum += value if node.to_play == to_play else -value
-            node.visit_count += 1
-            min_max_stats.update(node.reward + self.sts.discount * node.value().numpy().item())
-
-            value = node.reward + self.sts.discount * value
+def select_child(settings, node, min_max_stats):
+    """
+    Select the child with the highest UCB score.
+    """
+    #scores = [self.ucb_score(node, child, min_max_stats) for action, child in node.children.items()]
+    #exp = np.exp(scores - np.max(scores))
+    #prob = exp / exp.sum()
+    #action = np.random.choice([action for action, child in node.children.items()], p = prob)
+    #max_ucb = max([self.ucb_score(node, child, min_max_stats) for action, child in node.children.items()])
+    #action = np.random.choice([action for action, child in node.children.items() if self.ucb_score(node, child, min_max_stats) == max_ucb])
+    _, action, child = max((ucb_score(settings, node, child, min_max_stats), action, child) for action, child in node.children.items())
+    return action, node.children[action]
+    
+def ucb_score(settings, parent, child, min_max_stats):
+    """
+    The score for a node is based on its value, plus an exploration bonus based on the prior.
+    """
+    pb_c = (math.log((parent.visit_count + settings.pb_c_base + 1) / settings.pb_c_base) + settings.pb_c_init)
+    pb_c *= math.sqrt(parent.visit_count) / (child.visit_count + 1)
+ 
+    prior_score = pb_c * child.prior
+ 
+    if child.visit_count > 0:
+        value_score = min_max_stats.normalize(child.value())
+    else:
+        value_score = 0
+ 
+    return prior_score + value_score
 
 class Node:
     def __init__(self, prior):
@@ -122,35 +108,31 @@ class Node:
             return 0
         return self.value_sum / self.visit_count
 
-    def expand(self, actions, to_play, reward, policy_logits, hidden_state):
-        """
-        We expand a node using the value, reward and policy prediction obtained from the
-        neural network.
-        """
-        self.to_play = to_play
-        self.reward = reward
-        self.hidden_state = hidden_state
+def expand_node(node, actions, to_play, reward, policy_logits, hidden_state):
+    """
+    We expand a node using the value, reward and policy prediction obtained from the
+    neural network.
+    """
+    node.to_play = to_play
+    node.reward = reward
+    node.hidden_state = hidden_state
+    policy = {}
+    policy = {a: math.exp(policy_logits[0,a]) for a in actions}
+    policy_sum = sum(policy.values())
+    for action, p in policy.items():
+        node.children[action] = Node(p/policy_sum)
+    return node
 
-        policy = {}
-        for a in actions:
-            try:
-                policy[a] = 1 / sum(tf.exp(policy_logits[0] - policy_logits[0][a]))
-            except OverflowError:
-                print("Warning: prior has been approximated")
-                policy[a] = 0.0
-        for action, p in policy.items():
-            self.children[action] = Node(p)
-
-    def add_exploration_noise(self, dirichlet_alpha, exploration_fraction):
-        """
-        At the start of each search, we add dirichlet noise to the prior of the root to
-        encourage the search to explore new actions.
-        """
-        actions = list(self.children.keys())
-        noise = np.random.dirichlet([dirichlet_alpha] * len(actions))
-        frac = exploration_fraction
-        for a, n in zip(actions, noise):
-            self.children[a].prior = self.children[a].prior * (1 - frac) + n * frac
+def add_exploration_noise(node, settings):
+    """
+    At the start of each search, we add dirichlet noise to the prior of the root to
+    encourage the search to explore new actions.
+    """
+    actions = list(node.children.keys())
+    noise = np.random.dirichlet([settings.dirichlet_alpha] * len(actions))
+    frac = settings.exploration_fraction
+    for a, n in zip(actions, noise):
+        node.children[a].prior = node.children[a].prior * (1 - frac) + n * frac
 
 class MinMaxStats:
     """
@@ -170,7 +152,7 @@ class MinMaxStats:
             return (value - self.minimum) / (self.maximum - self.minimum)
         return value
 
-
+'''
 def select_action(node, temperature):
     """
     Select action according to the visit count distribution and the temperature.
@@ -190,8 +172,24 @@ def select_action(node, temperature):
         action = np.random.choice(actions, p=visit_count_distribution)
 
     return action
+'''
 
-def select_temperature(episode):
+def select_action(node, num_episodes, training = True):
+    """
+    After running simulations inside in MCTS, we select an action based on the root's children visit counts.
+    During training we use a softmax sample for exploration.
+    During evaluation we select the most visited child.
+    """
+    visit_counts = [child.visit_count for child in node.children.values()]
+    actions = [action for action in node.children.keys()]
+    if training:
+        temp = get_temperature(num_episodes)
+        action = softmax_sample(visit_counts, actions, temp)
+    else:
+        action, _ = max(node.children.items(), key=lambda item: item[1].visit_count)
+    return action
+
+def get_temperature(episode):
     if episode < 2000:
         temperature = 1.0
     elif episode < 4000:
@@ -201,3 +199,24 @@ def select_temperature(episode):
     else:
         temperature = 0.125
     return temperature
+
+def softmax_sample(visit_counts, actions, temp):
+    counts_exp = np.exp(visit_counts) * (1 / temp)
+    probs = counts_exp / np.sum(counts_exp, axis=0)
+    action_idx = np.random.choice(len(actions), p=probs)
+    return actions[action_idx]
+
+def simulate_and_select(settings, model, observation, num_episodes, action, training=True):
+    root = Node(0)
+    root_value, reward, policy_logits, hidden_state = model.initial_inference(np.expand_dims(observation, 0), training=training)
+    root_value = support_to_scalar(root_value, settings.support_size)
+    reward = reward.numpy().item()
+    root = expand_node(root, settings.action_pos, True, reward, policy_logits, hidden_state)
+    if training:
+        add_exploration_noise(root, settings)
+
+    # We then run a Monte Carlo Tree Search using only action sequences and the
+    # model learned by the networks.
+    root = run_mcts(settings, root, action, model, True, training=training)
+    action = select_action(root, num_episodes, training=training)
+    return action, root
